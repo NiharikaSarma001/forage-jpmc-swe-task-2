f69c2e3946df00f007ab8a055ecef62531ab80fe
Niharika Sarma <niharika.sarma0602@gmail.com>
2024-06-16 22:33:16 +0530

    feat: Enhance graph rendering and streaming data functionality
    - Added showGraph property to IState interface in App.tsx to control graph visibility
    - Modified App constructor to initialize showGraph to false, hiding the graph initially
    - Updated renderGraph method to conditionally render the graph based on showGraph state
    - Enhanced getDataFromServer method to continuously fetch data every 100ms after 'Start Streaming Data' button is clicked
    - Created custom.d.ts to declare 'perspective-viewer' as a valid JSX element and extended HTMLElement in PerspectiveViewerElement interface
    - Added PerspectiveViewerElement interface and extended HTMLElement in Graph.tsx to enable PerspectiveViewerElement behavior
    - Configured PerspectiveViewerElement with attributes: view (set to y_line), column-pivots, row-pivots, columns, and aggregates for proper graph rendering
    - Updated componentDidMount and componentDidUpdate methods in Graph.tsx to load and update the Perspective table with the server data
    - Adjusted tsconfig.json to include custom.d.ts for TypeScript type checking

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import './App.css';
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,  // Add showGraph property
 }
 
 /**
@@ -22,6 +23,7 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false,  // Initialize showGraph as false
     };
   }
 
@@ -29,18 +31,31 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    // Render the graph only if showGraph is true
+    if (this.state.showGraph) {
+      return (<Graph data={this.state.data}/>);
+    }
+    return null;
   }
 
   /**
    * Get new data from server and update the state with the new data
    */
   getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+    // Continuously get data from the server
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        // Update the state by creating a new array of data that consists of
+        // Previous data in the state and the new data from server
+        this.setState({
+          data: [...this.state.data, ...serverResponds],
+          showGraph: true,  // Set showGraph to true when data is received
+        });
+      });
+    }, 100);
+
+    // Optionally, you might want to stop fetching data at some point
+    // clearInterval(interval);
   }
 
   /**
@@ -54,12 +69,12 @@ class App extends Component<{}, IState> {
         </header>
         <div className="App-content">
           <button className="btn btn-primary Stream-button"
-            // when button is click, our react app tries to request
+            // when button is clicked, our react app tries to request
             // new data from the server.
             // As part of your task, update the getDataFromServer() function
             // to keep requesting the data every 100ms until the app is closed
             // or the server does not return anymore data.
-            onClick={() => {this.getDataFromServer()}}>
+            onClick={() => { this.getDataFromServer() }}>
             Start Streaming Data
           </button>
           <div className="Graph">
